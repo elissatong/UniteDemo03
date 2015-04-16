3e057b9477ec89afb48a05b6344a57abbff089fe
Elissa Tong <elissa.tong@gmail.com>
2015-04-16 17:29:03 +0800

    Initial commit
    Working BitRave Azure Mobile Services ToDoList for WP8 and PC. Windows
    Store Apps (WSA) build isn't working

---
+++ Builds/WP8/UniteDemo03/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+using UniteDemo03.Resources;
+
+namespace UniteDemo03
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
